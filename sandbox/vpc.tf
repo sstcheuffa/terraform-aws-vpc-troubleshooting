5dc766f2cb64455cf3ccb1f9edb7be51ab25e891
sstcheuffa <stcheuffa@alumni.harvard.edu>
2022-12-03 19:02:58 -0500

    Fixed errors & created exec plan

--- sandbox/vpc.tf
+++ sandbox/vpc.tf
@@ -31,14 +31,16 @@ variable "environment" {
 }
 
 module "sandbox_vpc" {
-  source = "../../modules/vpc"
+  source = "../modules/vpc"
 
   availability_zones        = var.availability_zones
   environment               = var.environment
   vpc_cidr_block            = "172.33.0.0/16"
   public_subnet_cidr_blocks = var.public_subnet_cidr_blocks
+  private_subnet_cidr_blocks = var.private_subnet_cidr_blocks
 }
 
 output "vpc_id" {
   value = module.sandbox_vpc.id
 }
+
